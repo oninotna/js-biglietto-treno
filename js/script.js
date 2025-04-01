f3743ae158d5d3354d99203614f6fe6aa857b3c1
oninotna <antoninomusarratubbi@gmail.com>
2025-04-01 18:59:14 +0200

    edit syntax

--- js/script.js
+++ js/script.js
@@ -6,22 +6,28 @@ const totalPrice = tripDistance * priceForKm;
 
 
 // INIZIO SINTASSI
-if ((tripDistance < 1 || agePassenger < 14) || (isNaN(tripDistance) || isNaN(agePassenger))) {
-    alert("inserisci i dati correttamente");
+if (isNaN(tripDistance) || isNaN(agePassenger)) {
+    alert("I dati inseriti devono essere di carattere numerico!!");
 }
-else if (tripDistance > 1 && agePassenger > 14) {
+else if (tripDistance < 1) {
+    alert("Non esiste una tratta cosi corta!!");
+}
+else if (agePassenger < 14) {
+    alert("Non hai l'età minima per poter viaggiare da solo!!");
+}
+else {
     if (agePassenger < 18) {
         const discountUn = (totalPrice * 20) / 100;
         const totalPriceUn = totalPrice - discountUn;
-        alert(`${"Ciao, sei un passegero minorenne, quindi il costo del tuo biglietto sarà scontato del 20%, quindi il totale è:"} ${totalPriceUn.toFixed(2)} ${"€"}`);
+        alert(`"Ciao, sei un passegero minorenne, quindi il costo del tuo biglietto sarà scontato del 20%, quindi il totale è:" ${totalPriceUn.toFixed(2)} "€"`);
     }
     else if (agePassenger > 65) {
         const discountOv = (totalPrice * 40) / 100;
         const totalPriceOv = totalPrice - discountOv;
-        alert(`${"Ciao, sei un passegero con età superiore ai 65 anni, quindi il costo del tuo biglietto sarà scontato del 40%, quindi il totale è:"} ${totalPriceOv.toFixed(2)} ${"€"}`);
+        alert(`"Ciao, sei un passegero con età superiore ai 65 anni, quindi il costo del tuo biglietto sarà scontato del 40%, quindi il totale è:" ${totalPriceOv.toFixed(2)} "€"`);
 
     }
     else {
-        alert(`${"Ciao, il totale del biglietto è è:"} ${totalPrice.toFixed(2)} ${"€"}`);
+        alert(`"Ciao, il totale del biglietto è:" ${totalPrice.toFixed(2)} "€"`);
     }
 }
